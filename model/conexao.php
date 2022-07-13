26fe790e42d08294610b8f0f445ffd393925241a
Ryan_AP <ryan.alvesportela@gmail.com>
2022-07-13 19:11:26 -0300

    local DB

--- model/conexao.php
+++ model/conexao.php
@@ -1,6 +1,8 @@
 <?php
 declare (strict_types = 1);
 
+
+
 final class Conexao
 {
     private static $instance;
@@ -17,16 +19,12 @@ final class Conexao
     {
     } 
     
-
     public static function getInstance() : PDO
     {
         if(!isset(self::$instance)) {
             try {
-                self::$instance = new PDO("mysql:host=micro-start.mysql.database.azure.com; dbname=microstart; charset=utf8", 'Micro_Start','Etec2022', array(
-                    PDO::MYSQL_ATTR_SSL_CA => 'cert/DigiCertGlobalRootCA.crt',
-                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                ),);
-            } 
+                self::$instance = new PDO("mysql:host=localhost; dbname=microstart; charset=utf8", 'root','root');
+            }
              catch (PDOException $erro) {
                 echo 'Erro no banco de dados: ' . $erro->getMessage();
             }
@@ -41,3 +39,6 @@ final class Conexao
     }
 
 }
+
+
+?>
